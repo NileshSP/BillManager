0a641f433d25413ba07ce0decbb0dc2b9b8076aa
Nilesh Patel <emailnileshp@gmail.com>
2021-03-11 16:12:28 -0500

    updated for json version

--- BillManagerApi/Startup.cs
+++ BillManagerApi/Startup.cs
@@ -33,18 +33,16 @@ namespace BillManagerApi
             services.AddLogging();
             services.AddMvc(options => options.EnableEndpointRouting = false)
                 .AddJsonOptions(options => options.JsonSerializerOptions.WriteIndented = true)
-                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+                .SetCompatibilityVersion(CompatibilityVersion.Latest);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IDBContext dBContext)
         {
-            if (env.EnvironmentName.ToLower().Contains("development"))
-            {
-                app.UseDeveloperExceptionPage();
-                dBContext.SeedSampleData().Wait();
-            }
-            else
+            app.UseDeveloperExceptionPage();
+            dBContext.SeedSampleData().Wait();
+
+            if (!env.EnvironmentName.ToLower().Contains("development"))
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
